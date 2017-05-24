f4550d2c1070e2de47a5e6d7bc3b701ee36273ee
simonrozsival <simon@rozsival.com>
2017-05-24 20:44:56 +0200

    :lipstick: Move private properties to the end of the class

--- Toggl.Ultrawave.Tests/Models/WorkspaceTests.cs
+++ Toggl.Ultrawave.Tests/Models/WorkspaceTests.cs
@@ -8,10 +8,37 @@ namespace Toggl.Ultrawave.Tests.Models
     {
         public class TheWorkspaceModel
         {
+            [Theory]
+            [MemberData("SerializationCases")]
+            public void CanBeSerialized(string validJson, Workspace validObject)
+            {
+                SerializationHelper.CanBeSerialized(validJson, validObject);
+            }
+            
+            [Theory]
+            [MemberData("DeserializationCases")]
+            public void CanBeDeserialized(string validJson, Workspace validObject)
+            {
+                SerializationHelper.CanBeDeserialized(validJson, validObject);
+            }
 
+            public static IEnumerable<object[]> SerializationCases
+                => new[]
+                {
+                    new object[] { completeJson, completeObject },
+                    new object[] { jsonWithNulls, objectWithNulls }
+                };
+
+            public static IEnumerable<object[]> DeserializationCases
+                => new[]
+                {
+                    new object[] { completeJson, completeObject },
+                    new object[] { jsonWithNulls, objectWithNulls },
+                    new object[] { jsonWithoutSomeFields, objectWithNulls }
+                };
 
             private const string completeJson =
-                "{\"id\":1234,\"name\":\"Default workspace\",\"profile\":0,\"premium\":false,\"business_ws\":false,\"admin\":true,\"SuspendedAt\":\"2018-04-24T12:16:48+00:00\",\"server_deleted_at\":\"2017-04-20T12:16:48+00:00\",\"default_hourly_rate\":0.0,\"default_currency\":\"USD\",\"only_admins_may_create_projects\":false,\"only_admins_see_billable_rates\":false,\"only_admins_see_team_dashboard\":false,\"projects_billable_by_default\":true,\"rounding\":0,\"rounding_minutes\":0,\"at\":\"2017-04-24T12:16:48+00:00\",\"logo_url\":\"https://assets.toggl.com/images/workspace.jpg\"}";
+                   "{\"id\":1234,\"name\":\"Default workspace\",\"profile\":0,\"premium\":false,\"business_ws\":false,\"admin\":true,\"SuspendedAt\":\"2018-04-24T12:16:48+00:00\",\"server_deleted_at\":\"2017-04-20T12:16:48+00:00\",\"default_hourly_rate\":0.0,\"default_currency\":\"USD\",\"only_admins_may_create_projects\":false,\"only_admins_see_billable_rates\":false,\"only_admins_see_team_dashboard\":false,\"projects_billable_by_default\":true,\"rounding\":0,\"rounding_minutes\":0,\"at\":\"2017-04-24T12:16:48+00:00\",\"logo_url\":\"https://assets.toggl.com/images/workspace.jpg\"}";
 
             private const string jsonWithNulls =
                 "{\"id\":1234,\"name\":\"Default workspace\",\"profile\":0,\"premium\":false,\"business_ws\":false,\"admin\":true,\"SuspendedAt\":null,\"server_deleted_at\":null,\"default_hourly_rate\":null,\"default_currency\":\"USD\",\"only_admins_may_create_projects\":false,\"only_admins_see_billable_rates\":false,\"only_admins_see_team_dashboard\":false,\"projects_billable_by_default\":true,\"rounding\":0,\"rounding_minutes\":0,\"at\":null,\"logo_url\":\"https://assets.toggl.com/images/workspace.jpg\"}";
@@ -62,35 +89,6 @@ namespace Toggl.Ultrawave.Tests.Models
                 At = null,
                 LogoUrl = "https://assets.toggl.com/images/workspace.jpg"
             };
-
-            [Theory]
-            [MemberData("SerializationCases")]
-            public void CanBeSerialized(string validJson, Workspace validObject)
-            {
-                SerializationHelper.CanBeSerialized(validJson, validObject);
-            }
-            
-            [Theory]
-            [MemberData("DeserializationCases")]
-            public void CanBeDeserialized(string validJson, Workspace validObject)
-            {
-                SerializationHelper.CanBeDeserialized(validJson, validObject);
-            }
-
-            public static IEnumerable<object[]> SerializationCases
-                => new[]
-                {
-                    new object[] { completeJson, completeObject },
-                    new object[] { jsonWithNulls, objectWithNulls }
-                };
-
-            public static IEnumerable<object[]> DeserializationCases
-                => new[]
-                {
-                    new object[] { completeJson, completeObject },
-                    new object[] { jsonWithNulls, objectWithNulls },
-                    new object[] { jsonWithoutSomeFields, objectWithNulls }
-                };
         }
     }
 }
