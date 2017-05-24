6b7dde8d60bd4f822fa2643da65b539e1f22d180
simonrozsival <simon@rozsival.com>
2017-05-24 20:42:24 +0200

    :construction: Use constants

--- Toggl.Ultrawave.Tests/Models/WorkspaceTests.cs
+++ Toggl.Ultrawave.Tests/Models/WorkspaceTests.cs
@@ -7,16 +7,18 @@ namespace Toggl.Ultrawave.Tests.Models
     {
         public class TheWorkspaceModel
         {
-            private string completeJson =>
+
+
+            private const string completeJson =
                 "{\"id\":1234,\"name\":\"Default workspace\",\"profile\":0,\"premium\":false,\"business_ws\":false,\"admin\":true,\"SuspendedAt\":\"2018-04-24T12:16:48+00:00\",\"server_deleted_at\":\"2017-04-20T12:16:48+00:00\",\"default_hourly_rate\":0.0,\"default_currency\":\"USD\",\"only_admins_may_create_projects\":false,\"only_admins_see_billable_rates\":false,\"only_admins_see_team_dashboard\":false,\"projects_billable_by_default\":true,\"rounding\":0,\"rounding_minutes\":0,\"at\":\"2017-04-24T12:16:48+00:00\",\"logo_url\":\"https://assets.toggl.com/images/workspace.jpg\"}";
 
-            private string jsonWithNulls =>
+            private const string jsonWithNulls =
                 "{\"id\":1234,\"name\":\"Default workspace\",\"profile\":0,\"premium\":false,\"business_ws\":false,\"admin\":true,\"SuspendedAt\":null,\"server_deleted_at\":null,\"default_hourly_rate\":null,\"default_currency\":\"USD\",\"only_admins_may_create_projects\":false,\"only_admins_see_billable_rates\":false,\"only_admins_see_team_dashboard\":false,\"projects_billable_by_default\":true,\"rounding\":0,\"rounding_minutes\":0,\"at\":null,\"logo_url\":\"https://assets.toggl.com/images/workspace.jpg\"}";
 
-            private string jsonWithoutSomeFields =>
+            private const string jsonWithoutSomeFields =
                 "{\"id\":1234,\"name\":\"Default workspace\",\"profile\":0,\"premium\":false,\"business_ws\":false,\"admin\":true,\"default_currency\":\"USD\",\"only_admins_may_create_projects\":false,\"only_admins_see_billable_rates\":false,\"only_admins_see_team_dashboard\":false,\"projects_billable_by_default\":true,\"rounding\":0,\"rounding_minutes\":0,\"logo_url\":\"https://assets.toggl.com/images/workspace.jpg\"}";
 
-            private Workspace completeObject => new Workspace
+            private static readonly Workspace completeObject = new Workspace
             {
                 Id = 1234,
                 Name = "Default workspace",
@@ -38,7 +40,7 @@ namespace Toggl.Ultrawave.Tests.Models
                 LogoUrl = "https://assets.toggl.com/images/workspace.jpg"
             };
 
-            private Workspace objectWithNulls => new Workspace
+            private static readonly Workspace objectWithNulls = new Workspace
             {
                 Id = 1234,
                 Name = "Default workspace",
