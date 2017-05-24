1c2e08e526402d27551d40f7ca14b9a786846990
simonrozsival <simon@rozsival.com>
2017-05-24 19:47:02 +0200

    :construction: Use serialization helper instead of the base model tests

--- Toggl.Ultrawave.Tests/Models/WorkspaceTests.cs
+++ Toggl.Ultrawave.Tests/Models/WorkspaceTests.cs
@@ -5,7 +5,7 @@ namespace Toggl.Ultrawave.Tests.Models
 {
     public class WorkspaceTests
     {
-        public class TheWorkspaceModel : BaseModelTests<Workspace>
+        public class TheWorkspaceModel
         {
             private string completeJson =>
                 "{\"id\":1234,\"name\":\"Default workspace\",\"profile\":0,\"premium\":false,\"business_ws\":false,\"admin\":true,\"SuspendedAt\":\"2018-04-24T12:16:48+00:00\",\"server_deleted_at\":\"2017-04-20T12:16:48+00:00\",\"default_hourly_rate\":0.0,\"default_currency\":\"USD\",\"only_admins_may_create_projects\":false,\"only_admins_see_billable_rates\":false,\"only_admins_see_team_dashboard\":false,\"projects_billable_by_default\":true,\"rounding\":0,\"rounding_minutes\":0,\"at\":\"2017-04-24T12:16:48+00:00\",\"logo_url\":\"https://assets.toggl.com/images/workspace.jpg\"}";
@@ -63,16 +63,16 @@ namespace Toggl.Ultrawave.Tests.Models
             [Fact]
             public void TestCanBeSerialized()
             {
-                CanBeSerialized(completeJson, completeObject);
-                CanBeSerialized(jsonWithNulls, objectWithNulls);
+                SerializationHelper.CanBeSerialized(completeJson, completeObject);
+                SerializationHelper.CanBeSerialized(jsonWithNulls, objectWithNulls);
             }
 
             [Fact]
             public void TestCanBeDeserialized()
             {
-                CanBeDeserialized(completeJson, completeObject);
-                CanBeDeserialized(jsonWithNulls, objectWithNulls);
-                CanBeDeserialized(jsonWithoutSomeFields, objectWithNulls);
+                SerializationHelper.CanBeDeserialized(completeJson, completeObject);
+                SerializationHelper.CanBeDeserialized(jsonWithNulls, objectWithNulls);
+                SerializationHelper.CanBeDeserialized(jsonWithoutSomeFields, objectWithNulls);
             }
         }
     }
